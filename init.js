d03862cf78ea379070d18ed1a057adf1408d020b
Renato GUILHERME DE SANT'ANNA SILVA <LR5972@engie.com>
2020-12-23 11:56:57 -0300

    - WhatsApp bot

---
+++ init.js
@@ -0,0 +1,22 @@
+const csv = require('csv-parser')
+const fs = require('fs');
+const wbm = require('./src/index');
+
+(async () => {
+    //Obtendo mensagem
+    const message = fs.readFileSync('msg.txt', 'utf8');    
+
+    //Obtendo contatos
+    let phones = [];
+    await fs.createReadStream('contatos.csv')
+            .pipe(csv())
+            .on('data', (data) => phones.push(data))
+            .on('end', async () => {
+                console.log(phones);
+                await wbm.start({showBrowser: true}).then(async () => {
+                await wbm.send(phones, message);
+                await wbm.end();
+                })
+            .catch(err => console.log(err));                
+    });
+})();
