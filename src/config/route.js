a5fc77fabcce4888b3318fe4c0b86f861d7a61e6
Renato GUILHERME DE SANT'ANNA SILVA <LR5972@engie.com>
2022-08-22 08:57:45 -0300

    feat: projeto web

--- src/config/route.js
+++ src/config/route.js
@@ -6,8 +6,15 @@ module.exports = class Route {
         return server;
     }
 
-    main(server){
-       //Tipo especialidade
+    main(server) {
+       
+        server.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
+
+        
         server.route('/qrCode').get(main.qrCode);
         server.route('/scrapper').post(main.scrapper);
         
